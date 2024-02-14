36239e878e6f307ccbddf3fbcdfb98b3920099a2
savemu <savemu@savemus-MacBook-Pro.local>
2024-02-14 14:53:40 +0100

    add migrations

--- django_passbook/urls.py
+++ django_passbook/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from django_passbook.views import register_pass, latest_version, registrations, log
 
 urlpatterns = [
-    path('^v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>/<slug:serial_number>', register_pass),
-    path('^v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>/<slug:serial_number>', register_pass),
-    path('^v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>', registrations),
-    path('^v1/passes/<slug:pass_type_id>/<slug:serial_number>', latest_version),
-    path('^v1/log$', log),
+    path('v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>/<slug:serial_number>', register_pass),
+    path('v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>/<slug:serial_number>', register_pass),
+    path('v1/devices/<slug:device_library_id>/registrations/<slug:pass_type_id>', registrations),
+    path('v1/passes/<slug:pass_type_id>/<slug:serial_number>', latest_version),
+    path('v1/log', log),
 ]
