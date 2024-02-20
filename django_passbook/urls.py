19b673814cf007a52985533c306f18ea3c74aa63
savemu <saver.mucci@gmail.com>
2024-02-20 12:45:38 +0100

    add trailing /

--- django_passbook/urls.py
+++ django_passbook/urls.py
@@ -16,9 +16,9 @@ class PassTypeConverter:
 register_converter(PassTypeConverter, "passtype")
 
 urlpatterns = [
-    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>/<slug:serial_number>', register_pass),
-    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>/<slug:serial_number>', register_pass),
-    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>', registrations),
-    path('v1/passes/<passtype:pass_type_id>/<slug:serial_number>', latest_version),
+    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>/<slug:serial_number>/', register_pass),
+    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>/<slug:serial_number>/', register_pass),
+    path('v1/devices/<slug:device_library_id>/registrations/<passtype:pass_type_id>/', registrations),
+    path('v1/passes/<passtype:pass_type_id>/<slug:serial_number>/', latest_version),
     path('v1/log', log),
 ]
